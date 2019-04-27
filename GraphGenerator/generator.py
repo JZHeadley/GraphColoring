334f4a245f3c56782514525fec3be2e9f7c31c5b
jzheadley <headleyjz@vcu.edu>
2019-04-27 18:42:45 -0400

    graph generator works

---
+++ GraphGenerator/generator.py
@@ -0,0 +1,17 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import sys
+numNodes = int(sys.argv[1])
+numEdges = int(sys.argv[2])
+G = nx.gnm_random_graph(numNodes,numEdges)
+# print(G.edges())
+with open('./graph.txt',"w") as file:
+    print(str(numNodes)+" "+str(numEdges))
+    file.write(str(numNodes)+" "+str(numEdges)+"\n")
+    for i,j in G.edges():
+        print(str(i) +" " + str(j)+"\n")
+        file.write(str(i) +" " + str(j)+"\n")
+    colors = " ".join(["0" for i in range(0, numNodes)])
+    print(colors)
+    file.write(colors)
+plt.show()
